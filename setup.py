65809892ecd0526df589c19d6eba22fbf6724ff6
Rohit Ahuja <rahuja95@gmail.com>
2016-11-25 12:24:49 -0500

    restructuring

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='messenger-api',
+    packages=['messenger-api'],
+    license='The MIT License (MIT)',
+    version='0.1.4',
+    description='Python client for FB Messenger Platform Bot',
+    long_description=open('README.md').read(),
+    author='Rohit Ahuja',
+    author_email='rahuja95@gmail.com',
+    url='https://github.com/rohitahuja',
+    keywords='facebook messenger api',
+    install_requires=['requests']
+)
